250d081958a725262489c6d35f3eba6034580825
JeyanArededon-UCM <arededonjeyan37@gmail.com>
2025-08-29 17:17:14 +0800

    changes of file
    Signed-off-by: JeyanArededon-UCM <arededonjeyan37@gmail.com>

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,10 @@
+// the single line
+public class Comments  
+{
+    public static void main(String[] args)
+    {
+         /* the mutli-line */
+        System.out.println("Hi, this is the 3-different comments");
+         /** the documentation */
+    }
+}
